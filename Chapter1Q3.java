b36da01fb64d111640a54d88b990429b22e186c7
Justin Yoon <yoons7@mcmaster.ca>
2021-05-04 23:51:49 -0400

    Q3

---
+++ Chapter1Q3.java
@@ -0,0 +1,16 @@
+public class CTCI {
+    public static void main(String[] args){
+
+        String stringA = "Mr John Smith";
+        System.out.println(URLify(stringA));
+
+    }
+    static String URLify(String a){
+        char[] content = a.toCharArray();
+        int i;
+
+        a = a.replace(" ","%20");
+        return a;
+    }
+}
+
